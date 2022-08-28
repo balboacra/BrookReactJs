1d375e1856b4bce13be576c0e0f9ec4e8a7ce9bf
balboacra <balboacra@gmail.com>
2022-08-28 13:19:14 -0300

    conLogo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,27 @@
 import CartWidget from "./CartWidget"
+import fish from "../assets/img/fish.png"
 
 const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-white shadow-sm">
             <div className="container-fluid">
-                <h2>FlyCast</h2>
+                <a className="navbar-brand" href="#">
+                    <img src={fish} className='mx-5' alt="logo" width="200"/>
+                </a>
                 <select className="form-select w-25 position-absolute top-50 start-50 translate-middle">
                     <option selected>Qué estás buscando?</option>
                     <option value="1">Cañas</option>
                     <option value="2">Combos</option>
                     <option value="3">Reels</option>
-                    <option value="3">Materiales de atado</option>
-                    <option value="3">Líneas</option>
-                    <option value="3">Indumentaria</option>
-                    <option value="3">Leaders, Tippets</option>
-                    <option value="3">Otros</option>
+                    <option value="4">Materiales de atado</option>
+                    <option value="5">Líneas</option>
+                    <option value="6">Indumentaria</option>
+                    <option value="7">Leaders, Tippets</option>
+                    <option value="8">Otros</option>
                 </select>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto">
-                        <li className="nav-item"><a className="nav-link text-dark" href="#">Productos</a></li>
+                        <li className="nav-item"><a className="nav-link text-dark" href="#">Excursiones</a></li>
                         <li className="nav-item"><a className="nav-link text-dark" href="#">Contacto</a></li>
                     </ul>
                     <CartWidget />
---
+++ src/components/logo.jsx
@@ -0,0 +1,8 @@
+import fish from '../assets/img/fish.png';
+
+const logo = () => {
+    return (
+        <img src={fish} />
+    )
+}
+export default logo
